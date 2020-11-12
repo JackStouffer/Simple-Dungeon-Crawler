0a97a0fc080e6ac7865dc0c2d9852b5b24bca31c
Jack Stouffer <jack@jackstouffer.com>
2020-11-12 17:41:52 -0500

    Formatting of rot js files

--- src/rot/path/path.ts
+++ src/rot/path/path.ts
@@ -1,28 +1,28 @@
 /**!
  * Copyright (c) 2012-now(), Ondrej Zara
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, 
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
- * 	* Redistributions of source code must retain the above copyright notice, 
+ *
+ * 	* Redistributions of source code must retain the above copyright notice,
  * 	  this list of conditions and the following disclaimer.
- * 	* Redistributions in binary form must reproduce the above copyright notice, 
- * 	  this list of conditions and the following disclaimer in the documentation 
+ * 	* Redistributions in binary form must reproduce the above copyright notice,
+ * 	  this list of conditions and the following disclaimer in the documentation
  * 	  and/or other materials provided with the distribution.
- * 	* Neither the name of Ondrej Zara nor the names of its contributors may be used 
- * 	  to endorse or promote products derived from this software without specific 
+ * 	* Neither the name of Ondrej Zara nor the names of its contributors may be used
+ * 	  to endorse or promote products derived from this software without specific
  * 	  prior written permission.
- * 			
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -32,7 +32,7 @@ export type ComputeCallback = (x: number, y: number) => any;
 export type PassableCallback = (x: number, y: number) => boolean;
 
 export interface Options {
-	topology: 4 | 6 | 8;
+    topology: 4 | 6 | 8;
 }
 
 /**
@@ -44,54 +44,59 @@ export interface Options {
  * @param {int} [options.topology=8]
  */
 export default abstract class Path {
-	_toX: number;
-	_toY: number;
-	_passableCallback: PassableCallback;
-	_options: Options;
-	_dirs: number[][];
+    _toX: number;
+    _toY: number;
+    _passableCallback: PassableCallback;
+    _options: Options;
+    _dirs: number[][];
 
-	constructor(toX: number, toY: number, passableCallback: PassableCallback, options: Partial<Options> = {}) {
-		this._toX = toX;
-		this._toY = toY;
-		this._passableCallback = passableCallback;
-		this._options = Object.assign({
-			topology: 8
-		}, options);
+    constructor(
+        toX: number,
+        toY: number,
+        passableCallback: PassableCallback,
+        options: Partial<Options> = {}
+    ) {
+        this._toX = toX;
+        this._toY = toY;
+        this._passableCallback = passableCallback;
+        this._options = Object.assign({
+            topology: 8
+        }, options);
 
-		this._dirs = DIRS[this._options.topology];
-		if (this._options.topology == 8) { /* reorder dirs for more aesthetic result (vertical/horizontal first) */
-			this._dirs = [
-				this._dirs[0],
-				this._dirs[2],
-				this._dirs[4],
-				this._dirs[6],
-				this._dirs[1],
-				this._dirs[3],
-				this._dirs[5],
-				this._dirs[7]
-			]
-		}
-	}
+        this._dirs = DIRS[this._options.topology];
+        if (this._options.topology === 8) { /* reorder dirs for more aesthetic result (vertical/horizontal first) */
+            this._dirs = [
+                this._dirs[0],
+                this._dirs[2],
+                this._dirs[4],
+                this._dirs[6],
+                this._dirs[1],
+                this._dirs[3],
+                this._dirs[5],
+                this._dirs[7]
+            ];
+        }
+    }
 
-	/**
-	 * Compute a path from a given point
-	 * @param {int} fromX
-	 * @param {int} fromY
-	 * @param {function} callback Will be called for every path item with arguments "x" and "y"
-	 */
-	abstract compute(fromX: number, fromY: number, callback: ComputeCallback): void;
+    /**
+     * Compute a path from a given point
+     * @param {int} fromX
+     * @param {int} fromY
+     * @param {function} callback Will be called for every path item with arguments "x" and "y"
+     */
+    abstract compute(fromX: number, fromY: number, callback: ComputeCallback): void;
 
-	_getNeighbors(cx: number, cy: number) {
-		let result = [];
-		for (let i=0;i<this._dirs.length;i++) {
-			let dir = this._dirs[i];
-			let x = cx + dir[0];
-			let y = cy + dir[1];
-			
-			if (!this._passableCallback(x, y)) { continue; }
-			result.push([x, y]);
-		}
-		
-		return result;
-	}
+    _getNeighbors(cx: number, cy: number) {
+        const result = [];
+        for (let i=0;i<this._dirs.length;i++) {
+            const dir = this._dirs[i];
+            const x = cx + dir[0];
+            const y = cy + dir[1];
+
+            if (!this._passableCallback(x, y)) { continue; }
+            result.push([x, y]);
+        }
+
+        return result;
+    }
 }
